048660707c1d65fd2d0f146577d4f81a2221016b
Angie <angiezhu05@gmail.com>
2024-04-25 00:45:55 -0700

    hello

--- screens/MainMapScreen/MainMapScreen.main.jsx
+++ screens/MainMapScreen/MainMapScreen.main.jsx
@@ -3,16 +3,25 @@ import { SafeAreaView, Text, Image, ScrollView} from "react-native";
 import { View, Button, StyleSheet } from 'react-native';
 import { styles } from "./MainMapScreen.styles";
 import MapView from 'react-native-maps';
+import { Marker } from "react-native-maps";
 
 
 export default function Map({navigation, route}) {
+    const TABLE_DATA = require("../../assets/movies.json");
 
-    const coordinates = [
+    const berkCoordinates = [
         { latitude: 37.9005, longitude: -122.3096 }, // Northwest corner
         { latitude: 37.9005, longitude: -122.2364 }, // Northeast corner
         { latitude: 37.8425, longitude: -122.2364 }, // Southeast corner
         { latitude: 37.8425, longitude: -122.3096 }, // Southwest corner
       ];
+
+    const testcoordinates = {
+        longitude: -122.26040999999999,
+        latitude: 37.870579999999997,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+    };
 
     const selectedLocationButton = () => {
         navigation.navigate("Locations")
@@ -27,9 +36,6 @@ export default function Map({navigation, route}) {
        },
        []
      );
-
-
-
     return (
         <SafeAreaView style={styles.container}>
             <MapView
@@ -41,6 +47,7 @@ export default function Map({navigation, route}) {
                 longitudeDelta: 0.1,
                 }}
                 >
+            <Marker coordinates={testcoordinates} />       
             </MapView>
         </SafeAreaView>
     );
